32f546cc1f682d1b2e648b1dbbed821ed7f67836
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-05-07 13:53:17 +0530

    Added dra and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kanbanify-yournewtab';
+  tasks: any;
+  constructor() {
+    if (localStorage.getItem('tasks') == null) {
+      let tasks = {
+        todo: [],
+        doing: [],
+        done: [],
+      };
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }
+
+  ngOnInit() {
+    console.log('nngonit');
+
+    function getStorage(): Observable<any> {
+      return of(JSON.parse(localStorage.getItem('tasks')));
+    }
+
+    getStorage().subscribe((val) => {
+      this.tasks = val;
+      console.log('done');
+    });
+    console.log('close ngonit');
+  }
+  onSubmit(task: NgForm) {
+    this.tasks.todo.push(task.value);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    task.reset();
+  }
+  delete(id: any) {
+    this.tasks.done.splice(id, 1);
+    console.log(this.tasks);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DragDropModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
